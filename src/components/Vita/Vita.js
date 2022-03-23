fab2ba6e921ff3a427a709443390190a66d728f7
Evij <=>
2022-03-23 18:32:11 +0100

    add real vita details

--- src/components/Vita/Vita.js
+++ src/components/Vita/Vita.js
@@ -3,18 +3,51 @@ import React from 'react';
 function Vita() {
   return (
     <div className='Vita'>
-      <h2>bio</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
-      </p>
+      
+      <h2>Helmut Mark</h2>
+      <p>1958 geboren in Österreich</p>
+      <h4>Ausstellungen (Auswahl)</h4>
+      <table>
+	<tbody>
+		<tr>
+			<td>2008</td>
+			<td>„Skulptur“, Kunstverein Medienturm, Graz</td>
+		</tr>
+		<tr>
+			<td>2002</td>
+			<td>Laden für Nichts, Leipzig</td>
+		</tr>
+		<tr>
+			<td>2000</td>
+			<td>„Helmut Mark : Heimo Zobernig. Snapshots : de nada“, Galerie Kunstbüro, Wien</td>
+		</tr>
+		<tr>
+			<td>1993</td>
+			<td>Galerie Waßermann, München Galerie Orms, Innsbruck</td>
+		</tr>
+		<tr>
+			<td>1990</td>
+			<td>Insam-Gleicher Gallery, Chicago (mit Robert Adrian) Galerie Arcade, Mödling, Galerie Orms, Innsbruck</td>
+		</tr>
+		<tr>
+			<td>1989</td>
+			<td>Galerie Grita Insam, Wien</td>
+		</tr>
+		<tr>
+			<td>1987</td>
+			<td>Galerie Amer, Wien<br />
+			Galerie Maria Bonk, Köln</td>
+		</tr>
+		<tr>
+			<td>1986</td>
+			<td>Galerie Amer, Wien</td>
+		</tr>
+		<tr>
+			<td>1985</td>
+			<td>Galerie Aufbau-Abbau, Berlin</td>
+		</tr>
+	</tbody>
+</table>
     </div>
   );
 }
